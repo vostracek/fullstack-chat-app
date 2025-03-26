a7f662cc0a0b4915ed5fc75d18c078cf904594df
vostracek <Lukas.vostry@solutionbox.cz>
2025-03-26 10:47:51 +0100

    Project Completed

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.get("/check", protectRoute, checkAuth)
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+export default router;
